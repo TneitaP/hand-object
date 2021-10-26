d3915a9b1daccd1a28625aee7838a2a39760ea22
binghui-z <z_binghui@163.com>
2021-10-27 00:19:55 +0800

    realize read all dataset

--- get_mask.py
+++ get_mask.py
@@ -1,18 +1,39 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
 # Code by Samarth Brahmbhatt
 
+import sys
+import os
 import argparse
-from loader.ContactPose.contactpose_dataset import ContactPose
+from loader.ContactPose.contactpose_dataset import ContactPose,get_object_names
 import loader.ContactPose.misc as mutils
 import loader.ContactPose.rendering as rutils
 import matplotlib.pyplot as plt
 import cv2
 import numpy as np
+from tqdm import tqdm
 
-#p_num, intent, object_name = 1, 'handoff', 'banana'
-#cp = ContactPose(p_num, intent, object_name)
+def get_all_contactpose(args):
+    """
+    Gets all participants and objects from ContactPose
+    Cuts out grasps with two hands or grasps using left hand
+    :return: list of (participant_num, intent, object_name, ContactPose_object)
+    """
+    samples = []
 
+    print('Reading ContactPose dataset')
+    for participant_id in tqdm(range(1,args.p_num)):
+        for intent in args.intent:
+            print(intent)
+            for object_name in get_object_names(participant_id, intent):                
+                cp = ContactPose(participant_id, intent, object_name)
+                if cp._valid_hands != [1]:  # If anything else than just the right hand, remove
+                    #print(object_name)   #delete "hand"、"palm_print"   以及  handoff:bowl、utah_teapot;   use:banana、bowl、camera、ps_controller、water_bottle
+                    continue              
+                samples.append((participant_id, intent, object_name, cp))
 
+    print('Valid ContactPose samples:', len(samples))
+
+    return samples
 def create_renderers(args,camera_name):
     # renderer for object mesh
     # note the mesh_scale, ContactPose object models are in units of mm 
@@ -28,7 +49,7 @@ def create_renderers(args,camera_name):
             hand_renderers.append(renderer)
     return {'object': object_renderer, 'hands': hand_renderers}
 
-def show_rendering_output(renderers, color_im, camera_name, frame_idx, crop_size=-1):
+def show_rendering_output(renderers, color_im, camera_name, frame_idx, save_path ,crop_size=-1,write=False,vis=False):
     joints = cp.projected_hand_joints(camera_name, frame_idx)
     if crop_size > 0:
         color_im, _ = mutils.crop_image(color_im, joints, crop_size)
@@ -55,43 +76,61 @@ def show_rendering_output(renderers, color_im, camera_name, frame_idx, crop_size
         color_im[mask] = mask_color
     both_hands_rendering = np.dstack(both_hands_rendering).max(2)
     
+    if write:
+        mask_result = color_im.copy()
+        object_rendering[object_rendering>0] = 255
+        both_hands_rendering[both_hands_rendering>0] = 255
+        mask_result[:,:,0] = object_rendering
+        mask_result[:,:,1] = both_hands_rendering
+        mask_result[:,:,2] = 0
+        cv2.imwrite(save_path,mask_result)
     """show""" 
-    ##opencv show
-    cv2.imshow("Masks",color_im[:, :, ::-1])
-    object_rendering[object_rendering>0] = 255
-    both_hands_rendering[both_hands_rendering>0] = 255
-    cv2.imshow("object_mask",object_rendering)
-    cv2.imshow("hand_mask",both_hands_rendering)
-    cv2.waitKey(0)
+    if vis:
+        ##opencv show
+        cv2.imshow("Masks",color_im[:, :, ::-1])
+        object_rendering[object_rendering>0] = 255
+        both_hands_rendering[both_hands_rendering>0] = 255
+        cv2.imshow("object_mask",object_rendering)
+        cv2.imshow("hand_mask",both_hands_rendering)
+        cv2.waitKey(0)
 
-    ##matplotlib show
-    # fig, (ax0, ax1, ax2) = plt.subplots(1, 3)
-    # ax0.imshow(color_im[:, :, ::-1])
-    # ax0.set_title('Masks')
-    # ax1.imshow(object_rendering)
-    # ax1.set_title('Object Depth')
-    # ax2.imshow(both_hands_rendering)
-    # ax2.set_title('Hand Depth')
-    # fig.suptitle(camera_name)  
-    # cv2.waitKey(0)
+        ##matplotlib show
+        # fig, (ax0, ax1, ax2) = plt.subplots(1, 3)
+        # ax0.imshow(color_im[:, :, ::-1])
+        # ax0.set_title('Masks')
+        # ax1.imshow(object_rendering)
+        # ax1.set_title('Object Depth')
+        # ax2.imshow(both_hands_rendering)
+        # ax2.set_title('Hand Depth')
+        # fig.suptitle(camera_name)  
+        # cv2.waitKey(0)
     
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description='create mask on contactpose')
-    parser.add_argument('--p_num', default=1, choices=['1','1-2'])
-    parser.add_argument('--intent',  default='handoff',choices=["use","handoff","use,handoff"])
+    parser.add_argument('--p_num', default=2, choices=['1','1-2'])
+    parser.add_argument('--intent',  default=["handoff","use"],type=str, choices=["use","handoff","use,handoff"])
     parser.add_argument('--object_name',default="banana", choices=["apple","banana","bowl"])
     parser.add_argument('--pen', action='store_true')
     parser.add_argument('--saveobj', action='store_true')
     parser.add_argument('--partial', default=-1, type=int, help='Only run for n samples')
     args = parser.parse_args()
 
-    cp = ContactPose(args.p_num, args.intent, args.object_name)
+    samples = get_all_contactpose(args)
+    crop_size = -1   #是否进行裁剪
 
-    frame_idx = np.random.choice(len(cp))
-    crop_size = -1
-    plt.close('all')
-    for camera_name in ('kinect2_left', 'kinect2_right', 'kinect2_middle'):
-        color_im = cv2.imread(cp.image_filenames('color', frame_idx)[camera_name])
-        renderers = create_renderers(args,camera_name)
-        show_rendering_output(renderers, color_im, camera_name, frame_idx, crop_size)
-    plt.show()
+    for idx in range(len(samples)):
+        cp = samples[idx][3]
+        for camera_name in ('kinect2_left', 'kinect2_right', 'kinect2_middle'):
+            image_root = os.path.join(samples[idx][3].data_tmp_dir,"images_full",camera_name,"color")
+            tmp = image_root.replace("images","mask")
+            label = 0
+            if not os.path.exists(tmp):
+                os.makedirs(image_root.replace("images","mask"))
+            #for frame_idx in next(os.walk(image_root))[2]:
+            for frame_idx, image_name in enumerate(tqdm(next(os.walk(image_root))[2])):
+                oriImage_path = os.path.join(image_root,image_name)
+                color_im = cv2.imread(oriImage_path)
+                renderers = create_renderers(args,camera_name)
+
+                mask_path = oriImage_path.replace("images","mask")
+                show_rendering_output(renderers, color_im, camera_name, frame_idx, mask_path, crop_size, write=True)
