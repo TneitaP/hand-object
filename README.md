f0c0ee54543dfd17fd149c45ce3a659aa1352c5f
binghui-z <z_binghui@163.com>
2021-10-27 16:40:03 +0800

    update code

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 数据集筛选：
 	palm_print:是手掌数据，没有对应的图片，故删除
 	hands:单纯双手动作，没有手物交互过程，故删除
+
+mask各通道顺序：
+	R—— object
+	G—— left_hand
+	B—— right_hand
--- loader/mask_contactpose.py
+++ loader/mask_contactpose.py
@@ -115,7 +115,7 @@ if __name__=="__main__":
     samples = get_all_contactpose(args)
     crop_size = -1   #是否进行裁剪
     print("begin")
-    for idx in range(len(samples)):
+    for idx in range(43,len(samples)):
         cp = samples[idx][3]
         for camera_name in ('kinect2_left', 'kinect2_right', 'kinect2_middle'):
             image_root = os.path.join(samples[idx][3].data_tmp_dir,"images_full",camera_name,"color")
